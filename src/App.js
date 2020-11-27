3f696f8ec714f2ebd305296d67b4153254faaf77
SakthiDhandapani <sakthi.dhandapani@gmail.com>
2020-11-27 19:27:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,8 @@ import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-do
 
 class App extends Component {
   render() {
-    console.log("Host URL"+process.env.PUBLIC_URL);
     return (
-
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
-        </header>
-          <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
-          </Switch>
-      </div>
-    </Router>
+      <p>Hello Milan this is React js world!</p>
     );
   }
 }
