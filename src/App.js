df96b40c541a504b6be8bd31b2f00ed381ee05a5
SakthiDhandapani <sakthi.dhandapani@gmail.com>
2020-11-30 17:11:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <p>Hello Milan Welcome to React World world!</p>
+      <p>Hello Milan Welcome to React World world Localzi.com!</p>
     );
   }
 }
